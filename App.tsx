d0b868dfdb714c9341a0b5570efc6f504fbb54ba
gabrielayresdev <gabriel.ar.fort@gmail.com>
2024-04-11 11:10:05 -0300

    feat: status bar

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
 import { ThemeProvider } from "styled-components/native";
 import Home from "./src/screens/Home/Home";
 import theme from "./src/themes/theme";
+import { StatusBar } from "react-native";
 
 export default function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Home />
-    </ThemeProvider>
+    <>
+      <StatusBar
+        translucent
+        barStyle="dark-content"
+        backgroundColor="transparent"
+      />
+      <ThemeProvider theme={theme}>
+        <Home />
+      </ThemeProvider>
+    </>
   );
 }
