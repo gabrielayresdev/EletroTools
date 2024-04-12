70db8cbe87954f0db52dbb810ea0da90bbe0c7e3
gabrielayresdev <gabriel.ar.fort@gmail.com>
2024-04-12 11:38:40 -0300

    feat: google fonts added

--- App.tsx
+++ App.tsx
@@ -3,15 +3,29 @@ import theme from "./src/themes/theme";
 import { StatusBar } from "react-native";
 import ResistorCalculator from "./src/screens/ResistorCalculator/ResistorCalculator";
 import { ResistorCalculatorContextProvider } from "./src/contexts/ResistorCalculatorContext";
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
   return (
     <>
       <StatusBar barStyle="dark-content" backgroundColor="#fff" />
       <ThemeProvider theme={theme}>
-        <ResistorCalculatorContextProvider>
-          <ResistorCalculator />
-        </ResistorCalculatorContextProvider>
+        {fontsLoaded && (
+          <ResistorCalculatorContextProvider>
+            <ResistorCalculator />
+          </ResistorCalculatorContextProvider>
+        )}
       </ThemeProvider>
     </>
   );
